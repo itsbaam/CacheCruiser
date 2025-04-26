90f4db8a747cdd28b82307fb24c2a4399651c387
baamf18 <cnmferreira18@gmail.com>
2025-04-26 14:23:41 -0300

    implement simple HTTP server

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
@@ -28,17 +29,37 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("Received %s response for %s\n", r.Method, r.URL.RequestURI())
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
+		fmt.Printf("Received %s request for %s\n", r.Method, r.URL.RequestURI())
+
+		targetUrl := *origin + r.URL.RequestURI()
+
+		fmt.Println("targetUrl", targetUrl)
+
+		resp, err := http.Get(targetUrl)
+		if err != nil {
+			http.Error(w, "Bad Gateway", http.StatusBadGateway)
+		}
+		defer resp.Body.Close()
+
+		for name, values := range resp.Header {
+			for _, value := range values {
+				w.Header().Add(name, value)
+			}
+		}
+
+		w.Header().Set("X-Cache", "MISS")
+
+		w.WriteHeader(resp.StatusCode)
+
+		if _, err := io.Copy(w, resp.Body); err != nil {
+			log.Printf("Error copying response body: %v", err)
+		}
 	})
 
 	addr := fmt.Sprintf(":%d", *port)
 
 	log.Printf("Starting CacheCruiser on %s, forwarding to %s", addr, *origin)
 	if err := http.ListenAndServe(addr, nil); err != nil {
-		log.Fatalf("Error starting CacheCruiser: %v", err)
+		log.Fatalf("Error starting CacheCruiser server: %v", err)
 	}
-
-	// log.Fatal(http.ListenAndServe(":8080", nil))
 }
